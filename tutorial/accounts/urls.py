925ef8462384e2e659fef1c7ad5f67c82c916a79
Your Name <muxazuxa@mail.ru>
2018-11-07 22:48:52 +0600

    33.Complete reset view . made own templates for them

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .import views
 from django.contrib.auth import views as auth_views
+from django.urls import reverse, reverse_lazy, resolve
 
 
 app_name="accounts"
@@ -12,9 +13,17 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.view_profile, name='view_profile'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
-    url(r'^change-password/$',views.change_password, name='change_password'),
-    url(r'reset-password/$', auth_views.PasswordResetView.as_view(template_name='accounts/reset_password.html'), name='password_reset'),
-    url(r'^reset-password/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    url(r'^reset-password/complete/$',auth_views.PasswordResetCompleteView.as_view(),name = 'password_reset_complete')
+    url(r'^change-password/$', views.change_password, name='change_password'),
+    url(r'^reset-password/$', auth_views.PasswordResetView.as_view(
+        template_name='accounts/reset_password.html',
+        success_url = reverse_lazy('accounts:password_reset_done'),
+        email_template_name ='accounts/reset_password_email.html'),
+        {'email_template_name':'accounts/reset_password_email.html'},
+        name='password_reset'),
+    url(r'^reset-password/done/$', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/reset_password_confirm.html',
+        success_url = reverse_lazy('accounts:password_reset_complete')
+    ), name='password_reset_confirm'),
+    url(r'^reset-password/complete/$', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name = 'password_reset_complete')
 ]
