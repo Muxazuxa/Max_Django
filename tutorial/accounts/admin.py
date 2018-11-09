fdfb182eb959084ff8532b8529909554c784834d
Your Name <muxazuxa@mail.ru>
2018-11-09 20:11:38 +0600

    38.Continiue Customize Admin Site

--- tutorial/accounts/admin.py
+++ tutorial/accounts/admin.py
@@ -4,4 +4,14 @@ from django.contrib import admin
 from accounts.models import UserProfile
 
 # Register your models here.
-admin.site.register(UserProfile)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city', 'website', 'phone', 'user_info')
+
+    def user_info(self, obj):
+        return obj.description
+
+    user_info.short_description = 'INFO'
+
+admin.site.register(UserProfile, UserProfileAdmin)
