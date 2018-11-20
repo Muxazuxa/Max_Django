baac6f5e95c0769b94bda0aa7969358d2ceb678d
Your Name <muxazuxa@mail.ru>
2018-11-20 21:48:52 +0600

    UserProfileManager model to filter Users

--- tutorial/accounts/models.py
+++ tutorial/accounts/models.py
@@ -5,6 +5,11 @@ from django.db.models.signals import post_save
 # Create your models here.
 
 
+class UserProfileManager(models.Manager):
+    def get_queryset(self):
+        return super(UserProfileManager, self).get_queryset().filter(city='London')
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete='')
     description = models.CharField(max_length=1000, default='')
@@ -12,6 +17,9 @@ class UserProfile(models.Model):
     website = models.URLField(default='')
     phone = models.IntegerField(default=0)
     image = models.ImageField(upload_to='profile_image', blank=True)
+
+
+    london=UserProfileManager()
 
     def __str__(self):
         return self.user.username
