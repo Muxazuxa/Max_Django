21d17f244a49bb3c10859b2e1031ca855ba97a67
Your Name <muxazuxa@mail.ru>
2018-11-24 22:39:48 +0600

    47.Post Form for posts

--- tutorial/home/views.py
+++ tutorial/home/views.py
@@ -1,5 +1,25 @@
 from django.views.generic import TemplateView
+from home.forms import HomeForm
+from django.shortcuts import render
 
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+    def get(self,request):
+        form = HomeForm()
+        return render(request, self.template_name,{'form':form})
+
+    def post(self,request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.user = request.user
+            post.save()
+            form.save()
+            text = form.cleaned_data['post']
+            form = HomeForm()
+        args = {'form':form, 'text':text}
+        return render(request, self.template_name, args)
+
+
